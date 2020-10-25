bc4bd549a8cdb9b13a12aa63821e6557f7802d16
Oscar Hermawan <oscar.hermawan90@gmail.com>
2020-10-25 12:03:03 +0800

    BackEnd - Setting ErroHandler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,10 +9,11 @@ import asyncHandler from 'express-async-handler'
 router.get(
   '/', 
   asyncHandler(async(req,res) => {
-  const products = await Product.find({})
-  
-  res.json(products)
-}))
+    const products = await Product.find({})
+    
+    res.json(products)
+  })
+)
 
 // @desc    Fetch single products
 // @route   GET /api/products/:id
@@ -20,13 +21,15 @@ router.get(
 router.get(
   '/:id', 
   asyncHandler(async(req,res) => {
-  const product = await Product.findById(req.params.id)
+    const product = await Product.findById(req.params.id)
 
-  if(product) {
-    res.json(product)
-  } else {
-    res.status(404).json({ message: 'Product not found' })
-  }
-}))
+    if(product) {
+      res.json(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+  })
+)
 
 export default router
