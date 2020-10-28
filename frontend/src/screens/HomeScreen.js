012124e2d8974cee12593be0a7c7b6437e8d82e8
Oscar Hermawan <oscar.hermawan90@gmail.com>
2020-10-28 10:04:23 +0800

    FrontEnd - Add Error Message, and Loader

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,15 +2,17 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { listProducts } from '../actions/productActions'
 import { Row, Col } from 'react-bootstrap'
-import axios from 'axios'
-
 import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+
 
 const HomeScreen = () => {
   const dispatch = useDispatch()
 
   const productList = useSelector(state => state.productList)
   const { loading, error, products } = productList
+
 
   useEffect(() => {
     dispatch(listProducts())
@@ -20,9 +22,9 @@ const HomeScreen = () => {
     <>
       <h1>Lates Products</h1>
       { loading ? (
-          <h2>loading...</h2>
+          <Loader />
         ) : error ? (
-          <h3>{error}</h3>
+          <Message variant='danger'>{error}</Message>
         ) : ( 
           <Row>
           {products.map(product => (
