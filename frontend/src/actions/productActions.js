7a83751c064786cfdc5c798c88b7609ffe21b523
Oscar Hermawan <oscar.hermawan90@gmail.com>
2020-10-31 18:14:20 +0800

    FrontEnd - Add Qty select and Add to cart button

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -30,7 +30,7 @@ export const listProducts = () => async (dispatch) => {
 }
 
 export const listProductDetails = (id) => async (dispatch) => {
-  console.log('kepanggil fe')
+  console.log('oke')
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST })
     const { data } = await axios.get(`/api/products/${id}`)
