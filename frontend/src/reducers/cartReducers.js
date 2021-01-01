5fc9775c62a355995a199a2e904c50ba3dd28c93
Oscar Hermawan <oscar.hermawan90@gmail.com>
2021-01-02 03:35:29 +0800

    FrontEnd - Add Cart Functionality, and fixing url in cartActions.js

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,7 @@
 import { CART_ADD_ITEM } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
+  console.log('masuk', action)
   switch(action.type) {
     case CART_ADD_ITEM:
       const item = action.payload
