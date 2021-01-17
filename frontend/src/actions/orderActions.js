d12c5cfb5565353ae0d77b0c196ab603f6fce1e7
Oscar Hermawan <oscar.hermawan90@gmail.com>
2021-01-17 23:14:55 +0800

    FrontEnd - [58] Fixing, wrong deleted line

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -50,7 +50,7 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
 
     const config = {
       headers: {
-        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`
       }
     }
 
