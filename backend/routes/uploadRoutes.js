b48fc5b8c397eaaef0bdcb6db6fadc5f4ac982c8
Oscar Hermawan <oscar.hermawan@activepipe.com>
2021-05-14 00:59:24 +0800

    BackEnd - Image Upload Config with multer

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,5 +1,5 @@
 import path from 'path'
-mport express from 'express'
+import express from 'express'
 import multer from 'multer'
 const router = express.Router()
 
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -23,8 +23,7 @@ import {
   USER_DELETE_FAIL,
   USER_UPDATE_REQUEST,
   USER_UPDATE_SUCCESS,
-  USER_UPDATE_FAIL,
-  USER_UPDATE_RESET
+  USER_UPDATE_FAIL
 } from '../constants/userConstants'
 import { ORDER_LIST_MY_RESET } from '../constants/orderContstants'
 
